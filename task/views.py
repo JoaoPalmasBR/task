40e08a2085dcda50c116d909c255034880138edc
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-02-20 21:06:14 -0300

    segundo commit

---
+++ task/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+class Tarefa(object):
+    def __init__(self, titulo, data):
+        self.titulo=titulo
+        self.data=data
+    def __str__(self):
+        return self.titulo
+
+def home(request):
+    return HttpResponse("Olá")
+def sobre(request):
+    return HttpResponse("Gustavo Siqueira")
+def tarefa(request, ano, mes, dia):
+    return HttpResponse("Tarefa: "+str(dia)+"/"+str(mes)+"/"+str(ano))
